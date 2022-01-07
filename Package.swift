7de2b2fb99bca20dcd45a094c7b2b554f527e904
Mahendra Rajput <mahendra.rajput@prefme.com>
2022-01-07 14:34:43 +0530

    - package file update

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MRKit",
-            url:"https://github.com/prefmemahendra/MRKit/releases/download/v-1.0.0/MRKit.xcframework.zip",
+            url:"https://github.com/prefmemahendra/MRKit/releases/download/v-1.0.0/MRKit-1.0.0.xcframework.zip",
             checksum: "e4610651f013961cc26d609bbd29c1361b250e7d87526b0d2e5fefe4e0703afc"
         )
     ])
